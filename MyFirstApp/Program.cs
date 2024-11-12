c4681289092e8141d67d9bbd3eeb82404fdaba59
Mari Loot <mari.loot@tktk.ee>
2024-11-12 23:53:57 +0200

    create new app

---
+++ MyFirstApp/Program.cs
@@ -0,0 +1,13 @@
+﻿//This is a comment to my code
+
+//Rakendus küsib kasutajal sisestada tema nime
+//Rakendus tervitab kasutajat nimepidi
+Console.WriteLine("Enter your name:"); //Output
+// String - sõne
+string userName = Console.ReadLine(); //Input
+
+
+Console.WriteLine("Hello" + ", " + userName + "!"); //Output
+
+// String interpolation
+Console.WriteLine($"Hello, {userName}!"); //Output
